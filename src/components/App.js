347348d0fc1ad499f80c38bb1e2f56ec2baeb41b
SRoy1997 <sarah@devpipeline.com>
2022-07-20 14:24:24 -0600

    changes to styles and add todo form

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello from App</h1>
-    </div>
-  );
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todo: "",
+      todos: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>To-Do List</h1>
+
+        <form className="add-todo">
+          <input type="text" placehoder="Add To-Do" />
+
+          <button>Add</button>
+        </form>
+
+        <div className="todo-item">
+          <input type="checkbox" className="checkbox" />
+
+          <p className="description">To-Do Item Description</p>
+
+          <button className="cancel-btn">X</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
